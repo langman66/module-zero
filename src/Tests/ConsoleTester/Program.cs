e7f9a84439898bdc6c8cb2b9aa996bd2a9d0b0e0
Chris Langston <chris@chrislangston.com>
2014-12-28 19:00:42 -0500

    analysis

--- src/Tests/ConsoleTester/Program.cs
+++ src/Tests/ConsoleTester/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Configuration;
 using System.Data.Entity;
+using System.Linq;
 using System.Reflection;
 using Abp;
 using Abp.Authorization.Roles;
 using Abp.Authorization.Users;
+using Abp.Configuration;
 using Abp.Dependency;
 using Abp.Domain.Entities;
 using Abp.Domain.Repositories;
@@ -20,9 +22,9 @@ namespace ConsoleTester
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(ConfigurationManager.ConnectionStrings["Default"].ConnectionString);
-            //Console.ReadLine();
-            //Test();
+            Console.WriteLine(ConfigurationManager.ConnectionStrings["Default"].ConnectionString);
+            Console.ReadLine();
+            Test();
         }
 
         private static void Test()
@@ -54,12 +56,17 @@ namespace ConsoleTester
         private readonly IRepository<User, long> _userRepository;
         private readonly MyUserStore _userStore;
         private readonly IRepository<MyEntity> _myEntityRepository;
+        private readonly IRepository<Setting, long> _userSettingRepository;
 
-        public Tester(IRepository<User, long> userRepository, MyUserStore userStore, IRepository<MyEntity> myEntityRepository)
+
+        public Tester(IRepository<User, long> userRepository, MyUserStore userStore, 
+            IRepository<MyEntity> myEntityRepository,
+            IRepository<Setting, long> userSettingRepository)
         {
             _userRepository = userRepository;
             _userStore = userStore;
             _myEntityRepository = myEntityRepository;
+            _userSettingRepository = userSettingRepository;
         }
 
         public void DoTests()
@@ -68,6 +75,23 @@ namespace ConsoleTester
             {
                 Console.WriteLine(user);
             }
+
+            foreach (var userSetting in _userSettingRepository.GetAllList())
+            {
+                Console.WriteLine(userSetting);
+            }
+
+            var newUserSetting = new Setting()
+            {
+                UserId = 1,
+                Name = "DefaultListSize",
+                Value = "25",   
+                CreationTime = DateTime.UtcNow 
+            };
+
+            _userSettingRepository.Insert(newUserSetting);
+
+
         }
     }
 
